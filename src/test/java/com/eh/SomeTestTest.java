c2f9cab57fa5322b3f76627325b82ab277ad2840
David Li <hzlienhui@corp.netease.com>
2016-04-09 17:11:31 +0800

    add test resources

--- src/test/java/com/eh/SomeTestTest.java
+++ src/test/java/com/eh/SomeTestTest.java
@@ -1,5 +1,7 @@
 package com.eh;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -10,6 +12,7 @@ import static org.testng.Assert.*;
  * Created by David Li on 2016/4/9.
  */
 public class SomeTestTest {
+    private Logger logger = LoggerFactory.getLogger(SomeTestTest.class);
     
     @BeforeMethod
     public void setUp() throws Exception {
@@ -23,6 +26,6 @@ public class SomeTestTest {
 
     @Test
     public void testMain() throws Exception {
-
+        logger.info("test");
     }
 }
