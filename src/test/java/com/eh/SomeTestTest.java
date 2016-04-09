00d22036eb881a7a7299b3b2a5cb5996d0efbfc3
David Li <hzlienhui@corp.netease.com>
2016-04-09 17:09:08 +0800

    add test folder

---
+++ src/test/java/com/eh/SomeTestTest.java
@@ -0,0 +1,28 @@
+package com.eh;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by David Li on 2016/4/9.
+ */
+public class SomeTestTest {
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testMain() throws Exception {
+
+    }
+}
